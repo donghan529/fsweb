2dd91fb1b39d5566e7ce0dac296ff6c3c0d2296b
donghan529 <dong529@naver.com>
2023-09-12 17:44:49 +0900

    0912

---
+++ 72_slide/슬라이드_data.js
@@ -0,0 +1,23 @@
+// 데이터 정의
+const imgObj = {
+    imgData1: {
+        imgSrc: 'images/img1.jpg',
+        imgAlt: '바다1'
+    },
+    imgData2: {
+        imgSrc: 'images/img2.jpg',
+        imgAlt: '바다2'
+    },
+    imgData3: {
+        imgSrc: 'images/img3.jpg',
+        imgAlt: '바다3'
+    },
+    imgData4: {
+        imgSrc: 'images/img4.jpg',
+        imgAlt: '바다4'
+    },
+    imgData5: {
+        imgSrc: 'images/img5.jpg',
+        imgAlt: '바다5'
+    }
+}
