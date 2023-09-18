555f85662b21e0468683ccd12156c11471bf4a92
donghan529 <dong529@naver.com>
2023-09-18 17:12:40 +0900

    0918

--- 70_project/project4/js/fullpage.js
+++ 70_project/project4/js/fullpage.js
@@ -1,20 +1,39 @@
 /* fullpage.js - layout */
-// try ~ catch -> 에러 처리
 const docEle = document.documentElement;
 const sec = document.querySelectorAll('.sec');
+const wh = window.innerHeight;
 
 for (let i = 0; i < sec.length; i++) {
     sec[i].onwheel = (e) => {
         // 기본 이벤트를 방지
         e.preventDefault();
         if (e.deltaY > 0) {
+            if (docEle.scrollTop > wh * (sec.length - 2) + 100) return;
             // 마우스휠 내림
             let next = e.currentTarget.nextElementSibling.offsetTop;
             docEle.scrollTop = next;
         } else {
             // 마우스휠 올림
-            let prev = e.currentTarget.previousElementSibling.offsetTop;
-            docEle.scrollTop = prev;
+            // HTML의 스크롤 값이 창의 높이 보다 작으면 wheel 이벤트 멈춤.
+            // if (docEle.scrollTop < wh) return;
+            /* 
+                try ~ catch 예외 처리
+                
+                try {
+                    정상 코드;
+                } catch(err) {
+                    에러 일때 코드;
+                } finally {
+                    무조건 실행할 코드;
+                }
+            */
+
+            try {
+                let prev = e.currentTarget.previousElementSibling.offsetTop;
+                docEle.scrollTop = prev;
+            } catch(err) {
+                alert('끝까지 올라왔습니다.')
+            }
         }
     };
 }
